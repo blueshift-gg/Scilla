844a5b9614e579769cbf3378704fb1b3d745ef39
BretasArthur1 <arthur.bretas@sou.inteli.edu.br>
2026-01-25 21:13:32 -0800

    feat: add build command supporting both upstream and solana toolchain

--- crates/scilla/src/commands/main_command.rs
+++ crates/scilla/src/commands/main_command.rs
@@ -3,9 +3,9 @@ use {
         commands::{Command, CommandFlow, navigation::NavigationSection},
         context::ScillaContext,
         prompt::{
-            prompt_account_section, prompt_cluster_section, prompt_config_section,
-            prompt_program_section, prompt_stake_section, prompt_transaction_section,
-            prompt_vote_section,
+            prompt_account_section, prompt_build_section, prompt_cluster_section,
+            prompt_config_section, prompt_program_section, prompt_stake_section,
+            prompt_transaction_section, prompt_vote_section,
         },
     },
     anyhow::Ok,
@@ -19,6 +19,7 @@ pub enum MainCommand {
     Program,
     Vote,
     Transaction,
+    Build,
     ScillaConfig,
     Exit,
 }
@@ -33,6 +34,7 @@ impl fmt::Display for MainCommand {
             MainCommand::Vote => "Vote",
             MainCommand::Transaction => "Transaction",
             MainCommand::ScillaConfig => "Scilla Config",
+            MainCommand::Build => "Build",
             MainCommand::Exit => "Exit",
         };
         f.write_str(label)
@@ -51,6 +53,7 @@ impl Command for MainCommand {
             MainCommand::Vote => prompt_vote_section()?.process_command(ctx).await?,
             MainCommand::Transaction => prompt_transaction_section()?.process_command(ctx).await?,
             MainCommand::Program => prompt_program_section()?.process_command(ctx).await?,
+            MainCommand::Build => prompt_build_section()?.process_command(ctx).await?,
             MainCommand::ScillaConfig => prompt_config_section()?.process_command(ctx).await?,
             MainCommand::Exit => {
                 return Ok(CommandFlow::Exit);
